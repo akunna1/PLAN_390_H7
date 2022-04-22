43fcf1001d55a870c9abd63a12b84a69f4b07a3b
Amarachi Akunna <akunna1@live.unc.edu>
2022-04-22 14:07:37 -0400

    Calculated the poverty percent in each tract and created plot

--- H7_script.R
+++ H7_script.R
@@ -14,27 +14,27 @@ acs_vars = load_variables(2019, "acs5")
 # The data is huge, so I am saving it to a file and view it in Excel.
 write_csv(acs_vars, "acsvars.csv")
 
-# Retrieve ACS data on the income levels of the households in Sacramento county in California
+# Retrieve ACS data on the income levels of the households in Sacramento county tract in California
 household_income = get_acs(
-  geography="county",  # could be tract, block group, etc.
+  geography="tract",  # could be tract, block group, etc.
   variables=c(
     "total_income"="B19001_001",
     "lessthan_10k"="B19001_002",
-    "10k_to_14.9k"="B19001_003",
-    "15k_to_19.9k"="B19001_004",
-    "20k_to_24.9k"="B19001_005",
-    "25k_to_29.9k"="B19001_006",
-    "30k_to_34.9k"="B19001_007",
-    "35k_to_39.9k"="B19001_008",
-    "40k_to_44.9k"="B19001_009",
-    "45k_to_49.9k"="B19001_010",
-    "50k_to_59.9k"="B19001_011",
-    "60k_to_74.9k"="B19001_012",
-    "75k_to_99.9k"="B19001_013",
-    "100k_to_124.9k"="B19001_014",
-    "125k_to_149.9k"="B19001_015",
-    "150k_to_199.9k"="B19001_016",
-    "200k_or_more"="B19001_017"
+    "i10k_to_14.9k"="B19001_003",
+    "i15k_to_19.9k"="B19001_004",
+    "i20k_to_24.9k"="B19001_005",
+    "i25k_to_29.9k"="B19001_006",
+    "i30k_to_34.9k"="B19001_007",
+    "i35k_to_39.9k"="B19001_008",
+    "i40k_to_44.9k"="B19001_009",
+    "i45k_to_49.9k"="B19001_010",
+    "i50k_to_59.9k"="B19001_011",
+    "i60k_to_74.9k"="B19001_012",
+    "i75k_to_99.9k"="B19001_013",
+    "i100k_to_124.9k"="B19001_014",
+    "i125k_to_149.9k"="B19001_015",
+    "i150k_to_199.9k"="B19001_016",
+    "i200k_or_more"="B19001_017"
   ),
   year=2019,
   state="CA",
@@ -47,14 +47,25 @@ View(household_income)
 # To do any spatial analysis, I have to join the household_income data to spatial information
 # Spatial information was obtained from: https://www.census.gov/geographies/mapping-files/time-series/geo/tiger-line-file.html
 
-# Download 2010 US County shapefile from the site, and load it into R.
-counties = read_sf("tl_2010_us_county10.shp")
-
-# Filtering just CA counties using the state FIPS (STATEFP10) code to do that
-ca_counties = filter(counties, STATEFP10=="06")
+# Download 2010 Sacramento county tract shapefile from the site, and load it into R.
+sacramento_county = read_sf("tl_2010_06067_tract10.shp")
 
 # renaming column GEOID column in household_income dataset to GEOID10
 household_income$GEOID10 <- household_income$GEOID
+household_income
 
 # Join the datasets together
-ca_counties = left_join(ca_counties, household_income, by="GEOID10")
+sacramento_county = left_join(sacramento_county, household_income, by="GEOID10")
+sacramento_county
+
+#Calculating the percentage of all households that make <$35,000/year in each tract
+sacramento_county$percent_under_35k = ((sacramento_county$lessthan_10kE + sacramento_county$i10k_to_14.9kE + sacramento_county$i15k_to_19.9kE + sacramento_county$i20k_to_24.9kE + sacramento_county$i25k_to_29.9kE + sacramento_county$i30k_to_34.9kE)/ sacramento_county$total_incomeE)*100
+sacramento_county$percent_under_35k
+
+# project the data to California State Plane
+sacramento_county = st_transform(sacramento_county,26943)
+
+# And map income poverty percent
+ggplot() +
+  geom_sf(data=sacramento_county, aes(fill=percent_under_35k)) +
+  scale_fill_viridis_c(option = "C")
